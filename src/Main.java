dbdb3299d74141f211c733b2ad66e4fbee51a110
Daniel-Olayinka <daniel.olayinka@gozade.com>
2026-08-20 16:41:20 +0100

    added Courses mangement with validation

--- src/Main.java
+++ src/Main.java
@@ -2,19 +2,43 @@ class Student{
     int id;
     String name;
     String email;
-
+    String registeredCourses;
 }
-class Course{
-    String code;
-    String title;
-    String unit;
-    String maximum;
+class Course {
+    private String code;
+    private String title;
+    private int unit;
+    private int maximumCapacity;
 
+    // Course management
+    Course(String code, String title, int unit, int maximumCapacity) {
+        this.code = code;
+        this.title = title;
+        this.unit = unit;
+        this.maximumCapacity = maximumCapacity;
+    }
+    public String getCode(){
+            return code;
+    }
+    public String getTitle(){
+        return title;
+    }
+    public int getUnit(){
+        return unit;
+    }
+    public int getMaximumCapacity(){
+        return maximumCapacity;
+    }
 
-
+    @Override
+    public String toString() {
+        return "Course Code = " + code + "\nTitle = " + title + "\nUnit = " + unit
+                + "\nCapacity = " + maximumCapacity;
+    }
 }
+
 class RegistrationService{
-
+     public void registerStudent(){}
 }
 
 public class Main {
