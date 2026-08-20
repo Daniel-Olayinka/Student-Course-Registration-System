69723c4ddbdf0c078184e99812b6d26fc361f223
Daniel <djolay@gmail.com>
2026-08-20 10:27:48 +0100

    My First commit

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+class Student{
+    int id;
+    String name;
+    String email;
+
+}
+class Course{
+    String code;
+    String title;
+    String unit;
+    String maximum;
+
+
+
+}
+class RegistrationService{
+
+}
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("Hello and welcome!");
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+        }
+    }
+}
